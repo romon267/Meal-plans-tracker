83f6d3fcd02b55b057fac82ba0238e1e060e3e71
unknown <romon267@gmail.com>
2021-01-27 20:56:57 +0300

    fix

--- planner/models.py
+++ planner/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
 class RecipeItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True, default=None)
     
-    grams = models.IntegerField(default=0, null=True, blank=True)
+    grams = models.IntegerField(null=True, blank=True)
 
     @property
     def get_total(self):
