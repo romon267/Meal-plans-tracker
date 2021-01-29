523a553cf06419a9871f302122362f587f7bb8cd
unknown <romon267@gmail.com>
2021-01-29 19:56:20 +0300

    small fix

--- django_tracker/settings.py
+++ django_tracker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DP_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['planartracker.herokuapp.com']
 
